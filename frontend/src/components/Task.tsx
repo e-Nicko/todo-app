c1c4d9a1fb370e63e774ee765d0e3c8b73ab1a51
Nickolai Yegorov <e-nicko@ya.ru>
2024-05-20 19:33:33 +0300

    fix: css corrections

--- frontend/src/components/Task.tsx
+++ frontend/src/components/Task.tsx
@@ -9,7 +9,7 @@ interface TaskProps {
 }
 
 const Task: React.FC<TaskProps> = ({ id, title, completed, onDelete, onToggleCompleted }) => (
-    <div>
+    <div className="Task">
         <input type="checkbox" checked={completed} onChange={() => onToggleCompleted(id)} />
         <span>{title}</span>
         <button onClick={() => onDelete(id)}>Delete</button>
